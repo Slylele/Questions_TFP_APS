db8eeadf190afc72620677cb8c50efdf2f9dee49
Slylele <57619777+Slylele@users.noreply.github.com>
2025-11-22 14:09:58 +0100

    Update qcm_uv_app.py

--- qcm_uv_app.py
+++ qcm_uv_app.py
@@ -155,14 +155,14 @@ for index, row in uv_questions.iterrows():
         user_choice = st.session_state.user_answers.get(question_key, "")
         # ✅ Affichage des réponses
         if user_choice == "Aucune sélection":
-            st.markdown(f"<span style='color:red'; margin:0; padding:0; line-height:1;>❌ Aucune sélection</span>", unsafe_allow_html=True)
+            st.markdown(f"<span style='color:red'; margin:0; padding:0; line-height:1;'>❌ Aucune sélection</span>", unsafe_allow_html=True)
         for opt_key, opt_text in options.items():
             if user_choice == opt_key and opt_key == correct_answer:
-                st.markdown(f"<span style='margin:0; padding:0; line-height:1;>✅ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
+                st.markdown(f"<span style='margin:0; padding:0; line-height:1;'>✅ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
             elif user_choice == opt_key and opt_key != correct_answer:
-                st.markdown(f"<span style='margin:0; padding:0; line-height:1; text-decoration: line-through;>❌ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
+                st.markdown(f"<span style='margin:0; padding:0; line-height:1; text-decoration: line-through;'>❌ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
             elif opt_key == correct_answer and user_choice != correct_answer:
-                st.markdown(f"<span style='margin:0; padding:0; line-height:1;>✅ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
+                st.markdown(f"<span style='margin:0; padding:0; line-height:1;'>✅ {opt_key} - {opt_text}</span>", unsafe_allow_html=True)
             else:
                 st.markdown(f"{opt_key} - {opt_text}")
 
